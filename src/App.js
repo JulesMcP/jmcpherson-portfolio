eeaa71b02d1a75bc75c4e54d4b5ced13f35df432
JulesMcP <rogue427@yahoo.com>
2022-05-31 09:43:06 -0500

    redesign & initialize tailwindcss

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from "react";
-import About from "./components/About";
-import Contact from "./components/Contact";
-import Nav from "./components/Nav";
-import Projects from "./components/Projects";
-import Skills from "./components/Skills";
 
-export default function App() {
+
+function App() {
     return (
-        <main className="text-gray-400 bg-gray-900 body-font">
-            <Nav />
-            <About />
-            <Projects />
-            <Skills />
-            <Contact />
-        </main>
+        <div>
+
+        </div>
     );
 }
+
+export default App;
