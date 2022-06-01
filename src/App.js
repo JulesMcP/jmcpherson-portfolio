d3c7aa6d3dc85fdcfbd3b9c1800777859aa00230
JulesMcP <rogue427@yahoo.com>
2022-05-31 22:30:46 -0500

    adds Skills section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import Contact from './components/Contact';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
 import About from './components/About';
 import Hero from './components/Hero';
 import Nav from './components/Nav';
@@ -8,6 +11,9 @@ function App() {
             <Nav />
             <Hero />
             <About />
+            <Skills />
+            <Projects />
+            <Contact />
 
         </div>
     );
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,2 +1,10 @@
-//import React from 'react';
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>Contact</div>
+    )
+}
+
+export default Contact
 
